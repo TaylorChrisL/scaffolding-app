d6304f660151cd2175841b0f35e13ea4209c101d
Chris Taylor <ctaylor534@gmail.com>
2022-07-21 14:34:32 -0700

    playing around with scaffolding in rails

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+class Post < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+ 
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+end
+end
